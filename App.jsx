bf55e6d5cf3d13dda30464176eb9344b71d29b27
Viktoria <vikiivanova1206@gmail.com>
2025-12-28 10:57:52 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BlogDetails from "./components/BlogDetails";
+import NotFound from "./components/NotFound";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
